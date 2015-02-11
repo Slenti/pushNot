1334f5215f68ed13af6d2b89b29d880587229150
Slenti <slenti@hotmail.de>
2015-02-11 15:11:27 +0100

    pushNot Basic
    Basic

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\AppProgrammin\pushNot\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+D:\AppProgrammin\pushNot\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\AppProgrammin\pushNot\platforms\android\ant-build\AndroidManifest.xml : \
+D:\AppProgrammin\pushNot\platforms\android\AndroidManifest.xml \
