2f6b59bdf9157152dd946c018bacfc1d16acd2dd
Slenti <slenti@hotmail.de>
2015-02-11 15:17:15 +0100

    d
    d

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,6 +1,6 @@
-D:\AppProgrammin\pushNot\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-D:\AppProgrammin\pushNot\platforms\android\ant-build\CordovaApp.ap_ \
-D:\AppProgrammin\pushNot\platforms\android\ant-build\classes.dex \
-D:\AppProgrammin\pushNot\platforms\android\CordovaLib\ant-build\classes.jar \
-D:\AppProgrammin\pushNot\platforms\android\libs\gcm.jar \
-D:\AppProgrammin\pushNot\platforms\android\libs\android-support-v13.jar \
+D:\AppProgrammin\pushNot\pushNot\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+D:\AppProgrammin\pushNot\pushNot\platforms\android\ant-build\CordovaApp.ap_ \
+D:\AppProgrammin\pushNot\pushNot\platforms\android\ant-build\classes.dex \
+D:\AppProgrammin\pushNot\pushNot\platforms\android\CordovaLib\ant-build\classes.jar \
+D:\AppProgrammin\pushNot\pushNot\platforms\android\libs\gcm.jar \
+D:\AppProgrammin\pushNot\pushNot\platforms\android\libs\android-support-v13.jar \
